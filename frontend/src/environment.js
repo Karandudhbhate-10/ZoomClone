2becf65e60d38aad350f14ef266fa33e383f1e88
karan dudhbhate <karandudhbhate2@gmail.com>
2025-12-16 19:04:24 +0530

    deploy

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,6 @@
 let IS_PROD = true;
-const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
-
-    "http://localhost:8000"
-
+const server = IS_PROD
+  ? "https://zoomclone-1-713e.onrender.com"
+  : "http://localhost:8000";
 
 export default server;
